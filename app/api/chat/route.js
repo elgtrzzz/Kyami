5bc1ecd84594969f659912e22a9f48b9e9bfedb6
elgtrzzz <elgtrzzpro@gmail.com>
2026-07-22 20:38:10 +0400

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -9,7 +9,7 @@ export async function POST(request) {
     );
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash"
+      model: "gemini-2.0-flash-lite""
     });
 
     const result = await model.generateContent(body.message);
